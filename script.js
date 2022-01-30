66da5201cec77f29c7cb1286a10bac694574d8be
Monta Pecina <montapecina@gmail.com>
2022-01-30 13:29:04 +0100

    update

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ window.onload=function(){
 
         let countTotal = elements.length;//count total
         let countRemaining = elements.length;//count remaining
-        let countDome = elements.length;
+        //let countDome = elements.length;//count done
 
         document.getElementById("count").innerHTML = countTotal;//count total
         document.getElementById("remaining_done").innerHTML = countRemaining;//count remaining
-        document.getElementById("count_done").innerHTML = countDome;//count done
+        //document.getElementById("count_done").innerHTML = countDome;//count done
         show();
     }
     
@@ -30,6 +30,7 @@ function addElement(){
             }else{
                 localStorage.setItem("todo-elements",JSON.stringify(elements));
             }
+
             let countTotal = elements.length;//count total
             document.getElementById("count").innerHTML = countTotal;//count total
             let countRemaining = elements.length;//count remaining
@@ -52,29 +53,28 @@ function show(){
 
 
 
-
 //check and strike task
 function strike(index){
-    if(elements[index].includes("<strike>")){
-    elements[index] = elements[index].replace("<strike>", ""); //remove strike
-    {
-        let countRemaining = elements.length;
-        document.getElementById("remaining_done").innerHTML = countRemaining;//count remaining
-    }
-    elements[index] = elements[index].replace("</strike>", ""); //remove strike
-    {
-        let countRemaining = elements.length;
-        document.getElementById("remaining_done").innerHTML = countRemaining;//count remaining
-    }
-    }else{
+    if(elements[index].includes("<strike>")){ //check if element is having strike throuh or not
+
+    elements[index] = elements[index].replace("<strike>", ""); //remove start tag strike, replace strike with nothing
+    elements[index] = elements[index].replace("</strike>", ""); //remove end tag of strike, replace strike with nothing
+        {
+            let countRemaining = elements.length;
+            document.getElementById("remaining_done").innerHTML = countRemaining;//count remaining
+            let countDome = elements.length - elements.length;//count done
+            document.getElementById("count_done").innerHTML = countDome;//count done
+        }
+    }else{//if element dont have strike, then add strike to element
     elements[index] = "<strike>" + elements[index] + "</strike>"; //put element in strike
-
-    let countRemaining = elements.length -1;
-    document.getElementById("remaining_done").innerHTML = countRemaining;//count remaining
-
-    let countDome = elements.length - (elements.length - 1);
-    document.getElementById("count_done").innerHTML = countDome;//count done
-
+        {
+            let countRemaining = elements.length -1;
+            document.getElementById("remaining_done").innerHTML = countRemaining;//count remaining
+            let countDome = elements.length - elements.length + 1;//count done
+            document.getElementById("count_done").innerHTML = countDome;//count done
+               
+        }
+ 
     }
     if(localStorage.getItem("todo-elements")==null){
         localStorage.setItem("todo-elements",JSON.stringify(elements));
@@ -87,14 +87,20 @@ function strike(index){
 
 //delete task
 function del(index){
-    elements.splice(index, 1);
+    elements.splice(index, 1);//remove one current element
     if(localStorage.getItem("todo-elements")==null){
         localStorage.setItem("todo-elements",JSON.stringify(elements));
     }else{
         localStorage.setItem("todo-elements",JSON.stringify(elements));
     }
+
     let countTotal = elements.length;//count total
     document.getElementById("count").innerHTML = countTotal;//count total
+    let countRemaining = elements.length;
+    document.getElementById("remaining_done").innerHTML = countRemaining;//count remaining
+    let countDome = elements.length - elements.length;//count done
+    document.getElementById("count_done").innerHTML = countDome;//count done
+
     show(); //refreshing 
 }
 
